15526c8e035bfb7f854783e898a0b3aa33f6388f
Igor Menezes <igorsilvamenezes.gmail.com>
2023-02-20 12:18:01 -0300

    Change that including a new structure nfc_device

--- nfc_device.c
+++ nfc_device.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <nfc_device.h>
 #include <usb_device.h>
-#include <usb.h>
 
 /*Prototypes*/
-struct usb_device* get_nfc_device(usb_device_list *usb_dev_list);
+nfc_device* get_nfc_device(usb_device_list *usb_dev_list);
 
 int nfc_init()
 {
@@ -21,30 +20,40 @@ int nfc_init()
         return res;
     }
     
-    nfc_device = get_nfc_device(device_list);
-    if(nfc_device){
-        printf("NFC device found at address %d\n", nfc_device->devnum);        
+    pnd = get_nfc_device(device_list);
+    if(pnd){
+        printf("NFC device found at address %d\n", pnd->pud->devnum);
     }else{
         printf("NFC device not found.\n");
         return 1;
     }
 }
 
-struct usb_device* get_nfc_device(usb_device_list *usb_dev_list)
+int nfc_open()
+{
+
+}
+
+nfc_device* get_nfc_device(usb_device_list *usb_dev_list)
 {
     usb_device_list *current;
-    struct usb_device *nfc_device = NULL;
+
+    nfc_device *new_device = malloc(sizeof(nfc_device));
+    if(!new_device){
+        return NULL;
+    }
     
     //Iterate through the list of USB devices and return the first nfc device
     for(current = usb_dev_list; current; current = current->next){
 
         if (current->device->descriptor.idVendor == ACR122_VENDOR_ID &&
             current->device->descriptor.idProduct == ACR122_PRODUCT_ID){
-                
-            nfc_device = current->device;
+
+            new_device->pud = current->device;
+
             break;
         }
     }
 
-    return nfc_device;
+    return new_device;
 }
--- nfc_device.h
+++ nfc_device.h
@@ -1,11 +1,22 @@
 #ifndef __NFC_DEVICE_H__
 #define __NFC_DEVICE_H__
 
+#include <usb.h>
+
 #define ACR122_VENDOR_ID 0x072f
 #define ACR122_PRODUCT_ID 0x2200
 #define DEVICE_NAME_LENGTH 256
 
-static struct usb_device *nfc_device = NULL;
+typedef struct {
+    char name[DEVICE_NAME_LENGTH];
+    const struct usb_device *pud;
+    usb_dev_handle *pudh;
+    uint32_t uiEndPointIn;
+    uint32_t uiEndPointOut;
+    uint32_t uiMaxPacketSize;    
+} nfc_device;
+
+static nfc_device *pnd = NULL;
 
 int nfc_init();
 
